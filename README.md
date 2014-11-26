a77dcaa85c01f85bd8862a1e1609e93aeb42761e
Shane Burkhart <shaneburkhart@gmail.com>
2014-11-26 21:54:09 +0000

    Finished the inital move to vanilla php.

--- README.md
+++
@@ -1,5 +0,0 @@
-# VE Design Group Website
-
-A website created with CakePHP.  Super basic.
-
-Needs a developers panel.
---
+++ html/developer_login.php
@@ -0,0 +1,18 @@
+<div class="container">
+	<div class="row">
+		<div class="span4 offset4">
+			<form class="form-signin" action="http://plansource.io/users/sign_in" method="post">
+			  <h2 class="form-signin-heading">Please sign in</h2>
+			  <div class="alert alert-error">
+			  	Invalid Login
+				</div>
+			  <input name="user[email]" type="text" class="input-block-level" placeholder="Email address">
+			  <input name="user[password]" type="password" class="input-block-level" placeholder="Password">
+			  <label class="checkbox">
+			    <input type="checkbox" value="remember-me"> Remember me
+			  </label>
+			  <button class="btn btn-large btn-primary" type="submit">Sign in</button>
+			</form>
+		</div>
+	</div>
+</div>
