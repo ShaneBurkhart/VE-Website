a38901430da16b50298ead3010e2ad11a3146c84
Shane Burkhart <shaneburkhart@gmail.com>
2015-08-07 15:56:55 -0500

    Restyled the about us section of about page.

--- html/about.php
+++ html/about.php
@@ -1,44 +1,91 @@
-<div class="about-container">
-  <div class="container">
-    <img src="/img/1.jpg" alt="">
-  </div>
-
-  <div class="about-overlay-container">
-    <div class="container">
-
-      <div class="about-overlay">
-        <div class="partnership">
-          <h2>
-            Partnership
-          </h2>
-          <p>
-            Michael Burkhart<br>
-            417.612.2913 x701<br>
-            mburkhart@vedesigngroup.com
-          </p>
-          <p>
-            Corry Worthington, P.E.<br>
-            417.612.2913 x702<br>
-            cworthington@vedesigngroup.com
-          </p>
-          <p>
-            We incorporate our experience in construction and development to provide an efficient design that will perform.  We value the collective experience of our team that specializes in multi-family and mixed use projects as our greatest asset.
-          </p>
+<div class="container">
+    <section>
+        <h1>Our Company</h1>
+        <img src="/img/clayton-night.jpg" width="450px" class="right company-info">
+        <p>
+            VE Design Group is a design management firm with in-house architecture and structural engineering. We specialize in mixed-use, multi-family housing as well as podium and parking garage structures for design-build developers.
+        </p>
+        <h4>Partners:</h4>
+        <ul>
+            <li>
+                <p><strong>Mike Burkhart</strong> &mdash; experienced in efficient construction and development to create a design that will perform.</p>
+            </li>
+            <li>
+                <p><strong>Corry Worthington</strong> &mdash; an Architectural Engineer with an emphasis in structures.</p>
+            </li>
+        </ul>
+        <p>With principal architect, Ron Ireland, and the rest of our design team, we have accumulated a strong history.</p>
+        <div class="clear">
+    </section>
+    <hr>
+    <div class="clear">
+        <h2>Our Projects</h2>
+        <div class="project">
+            <img src="/img/clayton-motor-court.jpg" width="600" class="left">
+            <ul>
+                <li><p>Retail + Multi-family (229 Units)</p></li>
+                <li><p>5 Story Apartments Over 2 Level Parking Structure</p></li>
+                <li><p>Clayton, MO</p></li>
+                <li><p>Under Construction</p></li>
+            </ul>
+            <div class="clear"></div>
         </div>
-        <div class="contact">
-          <h2>
-            Contact
-          </h2>
-          <p>
-            VE Design Group of MO, LLC<br>
-            520 E. Jackson Street, Suite 2<br>
-            Willard, MO 65781<br>
-            417.612.2913
-          </p>
+        <hr>
+        <div class="project">
+            <img src="/img/creve-coeur-rendering.jpg" width="600" class="left">
+            <ul>
+                <li><p>Multi-family (174 Units)</p></li>
+                <li><p>4 Story Apartments With 5 Level Parking Structure</p></li>
+                <li><p>Creve Coeur, MO</p></li>
+                <li><p>Under Construction</p></li>
+            </ul>
+            <div class="clear"></div>
         </div>
-        <div style="clear: both;">
-      </div>
-
+        <hr>
+        <div class="project">
+            <img src="/img/9th-elm.jpg" width="600" class="left">
+            <ul>
+                <li><p>Retail + Student Apartments (196 Beds)</p></li>
+                <li><p>3 Story Apartments Over Podium</p></li>
+                <li><p>Columbia, MO</p></li>
+                <li><p>Under Construction</p></li>
+            </ul>
+            <div class="clear"></div>
+        </div>
+        <hr>
+        <div class="project">
+            <img src="/img/tollgate.jpg" width="600" class="left">
+            <ul>
+                <li><p>Multi-family (201 Units)</p></li>
+                <li><p>3 Story Apartments With Surface Parking</p></li>
+                <li><p>Thompson's Station, TN</p></li>
+                <li><p>Under Construction</p></li>
+            </ul>
+            <div class="clear"></div>
+        </div>
+        <hr>
+        <div class="project">
+            <img src="/img/wku.jpg" width="600" class="left">
+            <ul>
+                <li><p>Student Apartments (237 Beds)</p></li>
+                <li><p>4 Story Apartments With Surface Parking</p></li>
+                <li><p>Bowling Green, KY</p></li>
+                <li><p>Phase 1 - Completed 2015</p></li>
+            </ul>
+            <div class="clear"></div>
+        </div>
+        <hr>
+        <div class="project">
+            <img src="/img/brookside-walnut.jpg" width="600" class="left">
+            <ul>
+                <li><p>Student Apartments (798 Beds)</p></li>
+                <li><p>4 Story Apartments With 5 Level Parking Structure</p></li>
+                <li><p>Columbia, MO</p></li>
+                <li><p>Phase 1 - Completed 2012</p></li>
+                <li><p>Phase 2 - Completed 2013</p></li>
+                <li><p>Phase 3 - Completed 2014</p></li>
+            </ul>
+            <div class="clear"></div>
+        </div>
     </div>
-  </div>
 </div>
